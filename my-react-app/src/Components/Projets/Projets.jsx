5d2f3e84207f25ae799782407be5ef4d539ca9f8
Nolwenn Bohelay <bohelay.nolwenn@gmail.com>
2025-02-28 11:58:49 +0100

    feat (filtres): ajout fonction filtre des projets

--- my-react-app/src/Components/Projets/Projets.jsx
+++ my-react-app/src/Components/Projets/Projets.jsx
@@ -1,20 +1,11 @@
-import { Link } from "react-router-dom"
 import PropTypes from "prop-types";
+import FilterButtons from "../Modals/Modals";
 
-function GenerateCards({ projets }) {
+function GenerateCards() {
     return (
         <div className="projets-container">
             <h2>Mes projets réalisés</h2>
-            <div className="projet-cards-container">
-            {projets.map((projet) => (
-                <div key={projet.id} className="projet-card">
-                    <Link to= {projet.link}>
-                    <img src={projet.cover} alt={projet.title} />
-                    <figcaption>{projet.title}</figcaption>
-                    </Link> 
-                  </div>
-            ))}
-            </div>
+            <FilterButtons />
             </div>
     );
 }
